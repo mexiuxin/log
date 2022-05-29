e9b1d72b78614fae613edd2a970a250345b10ef6
debian11 <debian11@debian.debian11.my>
2022-05-29 10:39:24 +0800

    submit script

---
+++ README.md
@@ -0,0 +1,19 @@
+# 经贸实习日志生成脚本
+### 运行环境debian10 or CentOS7    python3.6及以下
+
+### debian install
+    sudo apt install git python3 pip
+    
+### CentOS install
+    sudo yum install python3 git pip
+
+
+```
+pip3 install python-docx
+git clone https://github.com/mexiuxin/log.git
+cd log
+vim log_script
+修改信息，跟着模板走
+
+```
+
---
+++ log_script.py
@@ -0,0 +1,82 @@
+#!/usr/local/python36/bin/python3.6
+import os
+from docx import Document
+import random
+import linecache
+today=1
+test=1
+fileq=open('readfile','r')
+ppp=len(fileq.readlines())
+file = open('readfile','r')
+### 输入你的名字
+your_name="你的名字"
+rand_file = open('rand_readfile','r')
+file_line = len(rand_file.readlines()) 
+
+while(today<=120):
+    data = file.readline()
+    # file = open("") 
+
+    if(test==1):
+        os.system("cp ../init.docx "+your_name+"实习日志第"+str(today)+"天.docx")
+        doc = Document(your_name+"实习日志第"+str(today)+"天.docx")
+        tables = doc.tables
+        table0 = tables[0]
+        cell_02=table0.cell(0, 2)
+        cell_02.text=your_name
+
+        cell_02=table0.cell(0, 4)
+        cell_02.text="电子信息工程"
+
+        cell_02=table0.cell(1, 2)
+        cell_02.text="计算机网络"
+
+        cell_02=table0.cell(1, 4)
+        cell_02.text="19级"
+
+        cell_02=table0.cell(2, 2)
+        cell_02.text="2 月 17 日"
+
+        cell_02=table0.cell(2, 4)
+        cell_02.text="地区"
+
+        cell_02=table0.cell(3, 2)
+        cell_02.text="公司"
+
+        cell_02=table0.cell(3, 4)
+        cell_02.text="指导老师"
+        cell_02=table0.cell(4, 2)
+        cell_02.text="上班摸鱼工作给人跑腿"
+
+        cell_02=table0.cell(5, 2)
+        if(today == 1):
+            cell_02.text="今天至于我来说，具有特殊的意义;因为今天是我在晨源劳务公司实习的第一天，也是我为不久的将来迈向社会大舞台的第一步，心里紧张而又激动。古人云天行健以自强不息，地势坤以厚德载物，对未来的实习生活，我愿意付出极大的热诚和努力。"
+        elif(today <=ppp):
+            cell_02.text=data
+        else:
+            rand=random.randint(1, file_line)
+            cell_02.text=linecache.getline('rand_readfile', rand)
+
+
+
+            
+        #paragraphs = doc.paragraphs
+        #par0 = paragraphs[2]
+        # 获取段落文字
+        #par0_string = par0.text
+        #print(par0_string)
+
+        paragraphs = doc.paragraphs
+
+        i=0
+        par0 = paragraphs[2]
+        par0.text="第"+str(today)+"天"
+
+        doc.save(your_name+"实习日志第"+str(today)+"天.docx")
+
+    today=today+1
+
+
+
+
+
